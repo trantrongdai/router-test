90e62913f41b0492b6441693961a618c087865fa
trantrongdai <trongdai306@gmail.com>
2018-12-04 22:48:23 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
-class App extends Component {
-  render() {
+const MenuLink = ({ lable, to, activeOnlyWhenExact }) => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Route path={to} exact={activeOnlyWhenExact} children={({ match }) => {
+            var active = match ? 'active abc' : '';
+            return (
+                <li className={active}>
+                    <Link to={to} className="my-link">
+                        {lable}   
+                    </Link>
+                </li>
+            )
+        }}
+
+        />
     );
-  }
+}
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    {/**Menu */}
+                    <nav className="navbar navbar-inverse">
+                        <ul className="nav navbar-nav">
+                                <MenuLink lable="Home" to="/" activeOnlyWhenExact={true}/>
+                                <MenuLink lable="Contact" to="/contact" activeOnlyWhenExact={false}/>
+                                <MenuLink lable="About" to="/about" activeOnlyWhenExact={false}/>
+                        </ul>
+                    </nav>
+                    {/*Content*/}
+                    <Route path='/' exact component={Home} />
+                    <Route path='/contact' component={Contact} />
+                    <Route path='/about' component={About} />
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
